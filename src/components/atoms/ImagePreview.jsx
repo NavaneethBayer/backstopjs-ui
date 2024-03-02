1e3f6c1e818b34822f2a8853744567f0d4d10451
NavaneethVijay <sainavaneeth@live.in>
2024-03-03 00:39:20 +0530

    removing public from image src

--- src/components/atoms/ImagePreview.jsx
+++ src/components/atoms/ImagePreview.jsx
@@ -70,7 +70,8 @@ class ImagePreview extends React.Component {
   }
 
   render () {
-    let { hidden, settings, label, src } = this.props;
+    let { hidden, settings, label, src:imageSrc } = this.props;
+    let src = imageSrc.replace('public/', '');
     if (!src || src === '../..' || this.state.imgLoadError) {
       src = BASE64_PNG_STUB;
     }
